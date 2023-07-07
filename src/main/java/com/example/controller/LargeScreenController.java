0d30f0ae3b41920499494f3dcc0bdae12d628a13
t-rex <2973550363@qq.com>
2023-07-07 16:20:00 +0800

    feat: 大屏信息查看

--- src/main/java/com/example/controller/LargeScreenController.java
+++ src/main/java/com/example/controller/LargeScreenController.java
@@ -1,9 +1,16 @@
 package com.example.controller;
 
 
+import com.example.common.lang.Result;
+import com.example.entity.LargeScreen;
+import com.example.service.LargeScreenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/largeScreen")
 public class LargeScreenController {
+
+    @Autowired
+    private LargeScreenService largeScreenService;
+
+    @GetMapping("list")
+    public Result list(){
+        List<LargeScreen> list = largeScreenService.list();
+        return Result.success(list);
+    }
 
 }
