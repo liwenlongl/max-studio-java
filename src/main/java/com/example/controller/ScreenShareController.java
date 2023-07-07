1df067ed11d7a8c5c768f2c25aa07ff04ff761ad
t-rex <2973550363@qq.com>
2023-07-07 16:20:40 +0800

    feat: 大屏分享查看

--- src/main/java/com/example/controller/ScreenShareController.java
+++ src/main/java/com/example/controller/ScreenShareController.java
@@ -1,9 +1,16 @@
 package com.example.controller;
 
 
+import com.example.common.lang.Result;
+import com.example.entity.ScreenShare;
+import com.example.service.ScreenShareService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/screenShare")
 public class ScreenShareController {
+
+    @Autowired
+    private ScreenShareService screenShareService;
+
+    @GetMapping("/list")
+    public Result list(){
+        List<ScreenShare> list = screenShareService.list();
+        return Result.success(list);
+    }
 
 }
